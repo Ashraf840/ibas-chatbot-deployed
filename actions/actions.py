4a30ff789467c2a90ff6cf4c383f38a8aa30cb2b
Ashraf840 <shift2unleashed2011@gmail.com>
2023-09-05 12:26:50 +0600

    Initial commit: directory from deployment sevrer

--- actions/actions.py
+++ actions/actions.py
@@ -1,22 +1,13 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
-
-
-# This is a simple example for a custom action which utters "Hello World!"
-
 from multiprocessing import current_process
 from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker, FormValidationAction
-from rasa_sdk.events import SlotSet
+from rasa_sdk.events import SlotSet, EventType, SessionStarted, ActionExecuted, FollowupAction
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.types import DomainDict
-from .ticketing.ticketing_api import create_issue_v2
-
-
+from .ticketing.ticketing_api import create_issue_v2, get_project_list, get_category_list_by_project_oid, get_user_detail, redirect_to_cso_chatroom
+from .ticketing.ticketing_api import reqres,category_oid, issue_text
+import requests, json
 class ActionCreateIssue(Action):
     def name(self) -> Text:
         return "action_create_issue"
@@ -24,85 +15,308 @@ class ActionCreateIssue(Action):
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        user_name_bn = tracker.get_slot('user_name_bn')
-        user_name_en = tracker.get_slot('user_name_en')
-        user_address = tracker.get_slot('user_address')
-        user_email = tracker.get_slot('user_email')
-        issue_desc = tracker.get_slot('issue_desc')
+
+        # issue description detection
+        events = tracker.events
+        user_events = [e for e in events if e.get('event') == 'user']
+        print(" user events" ,len(user_events))
+        if len(user_events) < 2:
+            print("happenig <2")
+            return []
+        elif len(user_events) == 3:
+            previous_message = user_events[-2].get('text')
+            issue_desc = previous_message
+            print("issue", issue_desc)
+            print("happenig <3")
+        else: 
+            previous_message = user_events[-2].get('text')
+            issue_desc = previous_message
+            print("issue", issue_desc)
+            print("happenig <4")
+       
+        #Track sender id which is the socket id
         sender_id = tracker.sender_id
+
+        my_slot_value = tracker.latest_message.get("text")
+        category_oid = my_slot_value[8:]
+        print("category oid issue creation:", category_oid)
+
+        # Print the value of the slot
+        print("The value of the slot is:", my_slot_value)
+        print("The value of the trimmed slot is:", my_slot_value[8:])
+
+        # Get User detail of the loggedin user from IBAS 
+        response = requests.get(f'http://172.16.6.91/home/api/user-chatbot/socket/{sender_id}/')
+        data = response.json()
+        # return data
+
+        # data = get_user_detail()
+        print("data", data)
+
+        # Create issue and get response
         response = create_issue_v2(
-            mobile_number="01927040075",
-            issuer_name_bn=user_name_bn,
-            issuer_name_en=user_name_en,
-            address=user_address,
-            email=user_email,
-            issue_category_oid="ISSUE-OID-0003",
-            description=issue_desc,
-            issuer_oid="ISSUER-OID-0001",
-            sender_id=sender_id
+            mobile_number = data['phone'],
+            issuer_name_bn = data['user_name_bn'],
+            issuer_name_en = data['first_name'],
+            address = data['user_address'],
+            email = data['user_email'],
+            issue_category_oid = category_oid,
+            description = issue_desc,
+            issuer_oid = None,
+            sender_id = sender_id
         )
-        print(response)
+        print("response",response)
+
+        # If the connection is successful then redirect to the chatroom and connect with the chat operator
         if response['code'] == 200:
-            dispatcher.utter_message(text=f"আপনার সমসস্যাটি আমাদের অবগত করার জন্য ধন্যবাদ, শীঘ্রই আমাদের প্রতিনিধি আপনার সাথে যোগাযোগ করবেন")
+            dispatcher.utter_message(
+                text=f"আপনার সমসস্যাটি আমাদের অবগত করার জন্য ধন্যবাদ, শীঘ্রই আমাদের প্রতিনিধি আপনার সাথে যোগাযোগ করবেন")
+            # message = "[click here](https://www.facebook.com)"
+            # dispatcher.utter_message(text=message)
+            ################ hit api to redirect to the chatroom###################
+            
+            url = "http://172.16.6.91/home/api/user-chatroom/socket/"
+
+            payload = json.dumps({
+            "user_email": f"{data['user_email']}",
+	    "user_organization": f"{data['user_organization']}",
+            "location": f"{data['location']}",
+            "district": f"{data['district']}",
+            "division": f"{data['division']}",
+            "chatbot_socket_id": f"{sender_id}",
+            "issuerOid": f"{response['data']}"
+            })
+            headers = {
+            'Content-Type': 'application/json'
+            }
+            print("Inside if after the response 200")
+
+            # response = requests.request("POST", url, headers=headers, data=payload)
+            response = requests.post(
+                "http://172.16.6.91/home/api/user-chatroom/socket/", payload,
+                headers= headers
+            )
+            print("response 2", response)
+            message = response.content
+            cso_available = b'"CSO is avaiable! No msg was created yet!"'
+
+            if message == cso_available:
+                pass
+            else:
+                dispatcher.utter_message(text=message)
+            ########################################################
         else:
             dispatcher.utter_message(text=f"Error: Ticketing API ISSUE")
-            print(user_name_bn)
-            print(user_name_en)
-            print(user_address)
-            print(user_email)
+            print(data['user_name_bn'])
+            print(data['user_name_en'])
+            print(data['user_address'])
+            print(data['user_email'])
             print(issue_desc)
-        return [SlotSet("user_name_bn", None),
-                SlotSet("user_name_en", None),
-                SlotSet("user_address", None),
-                SlotSet("user_email", None),
-                SlotSet("issue_desc", None)]
 
 
-class ValidateBalanceQueryForm(FormValidationAction):
+        dispatcher.utter_message(
+                text=f"আপনার সমসস্যাটি আমাদের অবগত করার জন্য ধন্যবাদ, শীঘ্রই আমাদের প্রতিনিধি আপনার সাথে যোগাযোগ করবেন")
+        print("sender_ID:",sender_id)
+
+        print("Action create issue was called")
+        return []
+
+class ActionProjectCategory(Action):
+
     def name(self) -> Text:
-        return "validate_issue_creation_form"
+        return "action_show_project_category"
 
-    def validate_user_name_bn(
-            self,
-            slot_value: Any,
+    def run(self, dispatcher, tracker, domain):
+        buttons = []
+        # list = get_project_list()
+
+        # # Show button of the project category
+        # for project_data in list['data']:
+        #     buttons.append({
+        #       "title": project_data['project_title_bn'],
+        #       "payload": project_data['oid']
+        #     })
+
+        # dispatcher.utter_message("project id:  ", buttons = buttons)
+        print("action project category was called")
+
+        return []
+
+class ActionGetProjectOID(Action):
+    def name(self) -> Text:
+        return "action_get_project_oid"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        # payload = tracker.latest_message.get("payload")
+        # print("payload: ", payload)
+        project_oid= tracker.latest_message.get("text")
+        SlotSet("issue_description", project_oid)
+        print("project_oid:", project_oid)
+        issue_text(project_oid)
+        # if payload :
+        #     response = ActionCreateIssue().run(dispatcher, tracker, domain)
+        #     print(response)
+        #     dispatcher.utter_message("good")
+        # else :
+        #     pass
+
+        print('action get project oid called')
+
+        return []
+
+
+class ActionGetCategory(Action):
+    def name(self) -> str:
+        return "action_get_category_oid"
+
+    def run(self,
             dispatcher: CollectingDispatcher,
             tracker: Tracker,
-            domain: DomainDict,
-    ) -> Dict[Text, Any]:
-        return {"user_name_bn": slot_value}
+            domain: Dict[str, Any]) -> List[Dict[str, Any]]:
+        #test purpose class
+        category_oid= tracker.latest_message.get("text")
+        print("Project list Oid:", category_oid)
+        print("action get category oid was called")
+        return []
 
-    def validate_user_name_en(
-            self,
-            slot_value: Any,
+class ActionSetCategory(Action):
+    def name(self) -> str:
+        return "action_set_category_oid"
+
+    def run(self,
             dispatcher: CollectingDispatcher,
             tracker: Tracker,
-            domain: DomainDict,
-    ) -> Dict[Text, Any]:
-        return {"user_name_en": slot_value}
+            domain: Dict[str, Any]) -> List[Dict[str, Any]]:
+        #test purpose class
+        category_oid= tracker.latest_message.get("text")
+        SlotSet("category", category_oid)
+        print("Category Oid 2:", category_oid)
+        print("action set category oid was called")
+        return []
 
-    def validate_user_address(
-            self,
-            slot_value: Any,
+class ActionShowCategoryList(Action):
+    def name(self) -> str:
+        return "action_show_category_list"
+
+    def run(self,
             dispatcher: CollectingDispatcher,
             tracker: Tracker,
-            domain: DomainDict,
-    ) -> Dict[Text, Any]:
-        return {"user_address": slot_value}
+            domain: Dict[str, Any]) -> List[Dict[str, Any]]:
 
-    def validate_user_email(
-            self,
-            slot_value: Any,
-            dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: DomainDict,
-    ) -> Dict[Text, Any]:
-        return {"user_email": slot_value}
+        project_oid = tracker.latest_message.get("text")
+        buttons = []
+        project_oid = "16321052-ad11-4d4d-a8ec-4f6c0aeed310"
+        list = get_category_list_by_project_oid(project_oid)
+        for project_data in list['data']:
+            buttons.append({
+              "title": project_data['category_title_bn'],
+              "payload": 'category' + project_data['oid']
+            })
 
-    def validate_issue_desc(
-            self,
-            slot_value: Any,
-            dispatcher: CollectingDispatcher,
+        dispatcher.utter_message("আপনার সমস্যাটির শ্রেণী নির্বাচন করুনঃ ", buttons = buttons)
+        print("action_show_category_list")
+        category_oid = tracker.latest_message.get("text")
+        print("category oid", category_oid)
+
+        return []
+
+class ActionRedirectWebsite(Action):
+    def name(self) -> Text:
+        return "action_redirect_website"
+
+    def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
-            domain: DomainDict,
-    ) -> Dict[Text, Any]:
-        return {"issue_desc": slot_value}
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+      
+        # # set the URL of the website to redirect to
+        # website_url = "https://www.example.com"
+        
+        # # create a WebexteButton attachment with the website URL
+        # button = {"title": "Click here to visit the website", "url": website_url, "webexte": True}
+        
+        # # send a message with the button attachment
+        # dispatcher.utter_message(text="Redirecting to the website...", buttons=[button])
+
+        return []
+
+# class ActionCheckNid(Action):
+#     def name(self) -> Text:
+#         return "action_check_nid"
+
+#     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#         nid = tracker.get_slot("user_nid")
+#         print(nid)
+#             # data = get_user_detail()
+#             # actual_nid = data['user_NID_no']
+
+#             # while True:
+#             #     # retrieve user input from the slot
+#             #     user_nid = tracker.get_slot("user_nid")
+                
+#             #     # check if user input matches the actual nid
+#             #     if user_nid == actual_nid:
+#             #         dispatcher.utter_message("NID verified successfully.")
+#             #         break  # exit the loop if the user input is correct
+#             #     else:
+#             #         # dispatcher.utter_message("Invalid NID. Please try again.")
+#             #         user_nid = input("Invalid NID. Please enter your NID: ")
+#             #         # reset the slot value to None to prompt the user for input again
+#             #         return [SlotSet("user_nid", user_nid)]
+            
+#             # dispatcher.utter_message(intent="nid_validation_confirmation")
+#             # dispatcher.utter_message("utter_nid", tracker)
+#         # Validate NID against Django project database
+#         # If NID is valid, return a message with a link to the customer support chatroom
+#         # If NID is invalid, return a message asking the user to try again
+#         return []
+
+class ActionCheckNid(Action):
+    def name(self) -> Text:
+        return "action_check_nid"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        nid = tracker.get_slot("user_nid")
+        print(nid)
+        # Code to check if the user's NID is valid
+        # ...
+        data = get_user_detail()
+        actual_nid = data['user_NID_no']
+        print(actual_nid)
+        while True:
+            # retrieve user input from the slot
+            user_nid = tracker.get_slot("user_nid")
+            
+            # check if user input matches the actual nid
+            if user_nid == actual_nid:
+                dispatcher.utter_message("NID verified successfully.")
+                break  # exit the loop if the user input is correct
+            # elif user_nid == None:
+            #     dispatcher.utter_message("")
+            else:
+                # dispatcher.utter_message("Invalid NID. Please try again.")
+                dispatcher.utter_message(text="Invalid NID. Please enter your NID:")
+                ActionExecuted('action_listen')
+                # reset the slot value to None to prompt the user for input again
+                return [SlotSet("user_nid", None)]
+        # # If the NID is valid, show the project category buttons
+        # if nid:
+        #     # Get the list of projects
+        #     project_list = get_project_list()
+        #     # Create a list of button objects
+        #     buttons = []
+        #     for project_data in project_list['data']:
+        #         button = {
+        #             "title": project_data['project_title_bn'],
+        #             "payload": project_data['oid']
+        #         }
+        #         buttons.append(button)
+        #     # Use the dispatcher.utter_message() method to send a message to the user interface that includes the buttons
+        #     dispatcher.utter_message(text="আপনার সমস্যাটির শ্রেণী নির্বাচন করুনঃ", buttons=buttons)
+        # # If the NID is invalid, prompt the user to try again
+        # else:
+        #     dispatcher.utter_message("Invalid NID. Please try again.")
+
+        return []
--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
-nohup rasa run --enable-api --cors "*" >> chatbot.log &
-nohup rasa run actions >> actions.log &
+# nohup rasa run --enable-api --cors "*" >> chatbot.log &
+#!/bin/sh
+
+nohup rasa run --enable-api --cors "*" -p 5005 >> chatbot.log &
+nohup rasa run actions -p 5055 >> actions.log &
 tail -f chatbot.log
+
