3f1c90a6a96379c28b696aea8f5158f31d8209de
Ashraf840 <shift2unleashed2011@gmail.com>
2024-04-24 17:08:40 +0600

    Send the synthetic training progress value to django websocket infused with multi threading; The value is sent to the client end afterward to show the progress using the progress bar

--- run.sh
+++ run.sh
@@ -2,9 +2,13 @@
 
 # Run the Python script and capture its output
 python3 final_curator.py >> curator_log.txt 2>&1
+# Start the model training & simultaneously write the training log file
 rasa train >> train_log.txt 2>&1
-echo 'chiro@840' | sudo -S kill -9 $(sudo lsof -t -i:5005)
-echo 'chiro@840' | sudo -S kill -9 $(sudo lsof -t -i:5055)
+# Kill any orphaned service of Rasa after the training is completed
+echo 'ashraf826546' | sudo -S kill -9 $(sudo lsof -t -i:5005)
+echo 'ashraf826546@840' | sudo -S kill -9 $(sudo lsof -t -i:5055)
+# Start the rasa model server & write logs
 nohup rasa run --enable-api --cors "*" >> log.txt 2>&1
+# Start the rasa action server & write logs
 nohup rasa run actions >> actions.log &
 tail -f chatbot.log
--- training_automation.py
+++ training_automation.py
@@ -1,13 +1,48 @@
 from flask import Flask, jsonify
 import subprocess
+import asyncio
+import websockets
+import json
+import websocket
+import time
+import threading
+
 
 app = Flask(__name__)
+
+def send_message_to_django(message):
+    ws = websocket.WebSocket()
+    ws.connect("ws://127.0.0.1:8082/ws/chatbot-model/training/statistics/")  # WebSocket server address in Django
+    ws.send(message)
+    ws.close()
+
+
+def progress():
+    for progress in range(0, 101):
+        send_message_to_django(json.dumps(progress))
+        time.sleep(.2)
+
+
+class SendProgressThread(threading.Thread):
+    
+    def __init__(self):
+        threading.Thread.__init__(self)
+
+    def run(self):
+        progress()
+
 
 @app.route('/train_automation', methods=['GET'])
 def run_script():
     try:
-        # response_data = {'status': 'Training started'}
-        # return jsonify(response_data)
+        response_data = {'status': 'Training started'}
+
+        # Send a dummy progress data
+        # t = threading.Thread(target=progress())
+        # t.start()
+        SendProgressThread().start()
+
+        return jsonify(response_data)
 
         # Run the 'run.sh' script using subprocess
         result = subprocess.run(['./run.sh'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
@@ -26,4 +61,4 @@ def run_script():
         
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5010)
